285fa73e7359d3f8fb938324ad939e88aca14295
yavuzyilmaz <yavuzyilmaz@850@gmail.com>
2025-09-03 03:04:05 +0300

    feat: implement advanced data structures - set exercises

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
-# Alıştırma 1
-kisisel_bilgiler = {
-    "isim": "Yavuz",
-    "yas": 39,
-    "sehir": "İstanbul"
-}
+# Alıştırma 1 (Kolay): Tekrar Edenleri Temizle
 
-kisisel_bilgiler["meslek"] = "Öğretmen"
-kisisel_bilgiler["yas"] = kisisel_bilgiler["yas"] + 1
-del kisisel_bilgiler["sehir"]
-print(kisisel_bilgiler)
+siparis_kodlari = [101, 102, 103, 101, 104, 102, 105, 106, 103]
+print(f"Veri Kümesi: {set(siparis_kodlari)}, Veri Tipi: {type(set(siparis_kodlari))}")
+
+python_kursu = {"Ali", "Veli", "Ayşe", "Fatma", "Can"}
+web_gelistirme_kursu = {"Zeynep", "Buse", "Ali", "Can", "Mehmet"}
+
 
 # Alıştırma 2
 
-stok_durumu = {"elma": 50, "muz": 30, "portakal": 45}
-urun_adi = input("Hangi ürünün stok durumunu bilmek istersiniz?")
 
-stok_miktari = stok_durumu.get(urun_adi)
-if stok_miktari:
-    print(f"{urun_adi} ürününden stokta {stok_miktari} adet bulunmaktadır.")
-else:
-    print(f"Üzgünüz, {urun_adi} stoklarımızda bulunmamaktadır.")
+def yazdir(kume: set, baslik: str) -> None:
+    print(baslik)
+    print("-" * 25)
+    for ogrenci in kume:
+        print(ogrenci)
+    print("-" * 25)
+    print("\n")
 
-for urun, stok in stok_durumu.items():
-    print(f"{urun} --> {stok} adet")
+
+kesisim_kurs = python_kursu.intersection(web_gelistirme_kursu)
+birlesim_kurs = python_kursu.union(web_gelistirme_kursu)
+yalniz_python = python_kursu.difference(web_gelistirme_kursu)
+
+yazdir(kesisim_kurs, "Her iki kursa birden katılan öğrenciler:")
+yazdir(birlesim_kurs, "Her iki kursa birden katılan öğrenciler:")
+yazdir(yalniz_python, "Yalnız python kursuna katılan öğrenciler:")
+
 
 # Alıştırma 3
-cumle = "merhaba dünya"
-harf_frekanslari = {}
-for harf in cumle:
-    if harf == " ":
-        continue
-    if harf not in harf_frekanslari:
-        harf_frekanslari[harf] = 1
-    else:
-        harf_frekanslari[harf] += 1
 
-print(harf_frekanslari)
+def pangram_tester(cumle: str) -> None:
+    """
+    Verilen cümlenin pangram olup olmadığını test eder.
+    Args:
+        cumle(str):
+
+    Returns:
+        None
+
+    """
+    alfabe = set("abcdefghijklmnopqrstuvwxyz")
+    set(cumle.lower())
+    cumle_kumesi = set()
+
+    for harf in set(cumle.lower()):
+        if harf == " ":
+            continue
+        else:
+            cumle_kumesi.add(harf)
+
+    fark_kumesi = alfabe.difference(cumle_kumesi)
+    if not fark_kumesi:
+        print("Cümle bir pangramdır.")
+    else:
+        print(f"Cümle bir pangram değildir.Eksik olan harfler:")
+        for harf in fark_kumesi:
+            print(f"--{harf}--")
+
+
+cumle = "the quick brown fox jumps over the lazy dog"
+cumle2 = "merhaba dunya"
+pangram_tester(cumle)
+pangram_tester(cumle2)
